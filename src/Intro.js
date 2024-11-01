918925ac3acbebad76c71d288124563397d643c9
flownumber <flownumber458@gmail.com>
2024-11-01 14:34:49 +0100

    daje1

--- src/Intro.js
+++ src/Intro.js
@@ -12,7 +12,7 @@ function Intro() {
           <img src="img/front1.jpg" alt="Dolce 2" />
           <img src="img/front2.jpg" alt="Dolce 3" />
           <img src="img/front3.jpg" alt="Dolce 4" />
-          <img src="img/front4.jpg" alt="Dolce 5" />
+          <img src="img/front2.jpg" alt="Dolce 5" />
           <img src="img/front5.jpg" alt="Dolce 6" />
           <img src="img/front6.jpg" alt="Dolce 7" />
           <img src="img/front.jpg" alt="Dolce 8" />
