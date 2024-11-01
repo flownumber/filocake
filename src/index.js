05447a94697717a9ab0d9a82c5a51597693c1e8b
flownumber <flownumber458@gmail.com>
2024-11-01 20:51:25 +0100

    daje2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Importa createRoot
-import Main from './Main'; // Importa Main
+import App from './App'; // Importa Main
 import './index.css'; // Importa eventuali stili globali
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Crea il root
 root.render(
   <React.StrictMode>
-    <Main /> {/* Monta Main qui */}
+    <App /> {/* Monta Main qui */}
   </React.StrictMode>
 );
