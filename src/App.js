7902aa7ae26c7bc88309323ef6ba9f5c13e00422
flownumber <flownumber458@gmail.com>
2024-11-01 20:57:02 +0100

    daje2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import Intro from './Intro';
-import Products from './Products';
-import OrderForm from './OrderForm';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
-      
-      <Intro />
-      <Products />
-      <OrderForm />
-      
+           
     </div>
   );
 }
