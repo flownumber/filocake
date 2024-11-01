be3e91a071542408be9bd6062aedaa3aca42bc74
flownumber <flownumber458@gmail.com>
2024-11-01 21:31:52 +0100

    daje2

--- src/LactoseFreePage.js
+++ src/LactoseFreePage.js
@@ -2,15 +2,14 @@
 import React from 'react';
 import './LactoseFreePage.css';
 
-// Importa le immagini
-import biscottiSenzaLattosioImage from 'img/biscotti-senza-lattosio.jpg';
-import tortaSenzaLattosioImage from 'img/torta-senza-lattosio.jpg';
+// Importa le immagini usando il percorso corretto
+import biscottiSenzaLattosioImage from './img/biscotti-senza-lattosio.jpg';
+import tortaSenzaLattosioImage from './img/torta-senza-lattosio.jpg';
 
 const LactoseFreePage = () => {
   const lactoseFreeProducts = [
     { id: 1, name: 'Biscotti Senza Lattosio', description: 'Deliziosi biscotti senza lattosio.', price: '€10.00', image: biscottiSenzaLattosioImage },
     { id: 2, name: 'Torta Senza Lattosio', description: 'Torta leggera e soffice senza lattosio.', price: '€25.00', image: tortaSenzaLattosioImage },
-    // Aggiungi altri prodotti senza lattosio
   ];
 
   return (
