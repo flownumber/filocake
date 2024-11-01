ff4b434f3529d4db7e48edfa03531d8850ec7b7d
flownumber <flownumber458@gmail.com>
2024-11-01 11:24:56 +0100

    terzamodifica

--- src/Intro.js
+++ src/Intro.js
@@ -8,14 +8,14 @@ function Intro() {
       <div className="image-carousel">
         {/* Contenitore per le immagini scorrevoli */}
         <div className="carousel-track">
-          <img src="/img/front.jpg" alt="Dolce 1" />
-          <img src="/img/front1.jpg" alt="Dolce 2" />
-          <img src="/img/front2.jpg" alt="Dolce 3" />
-          <img src="/img/front3.jpg" alt="Dolce 4" />
-          <img src="/img/front4.jpg" alt="Dolce 5" />
-          <img src="/img/front5.jpg" alt="Dolce 6" />
-          <img src="/img/front6.jpg" alt="Dolce 7" />
-          <img src="/img/front.jpg" alt="Dolce 8" />
+          <img src="img/front.jpg" alt="Dolce 1" />
+          <img src="img/front1.jpg" alt="Dolce 2" />
+          <img src="img/front2.jpg" alt="Dolce 3" />
+          <img src="img/front3.jpg" alt="Dolce 4" />
+          <img src="img/front4.jpg" alt="Dolce 5" />
+          <img src="img/front5.jpg" alt="Dolce 6" />
+          <img src="img/front6.jpg" alt="Dolce 7" />
+          <img src="img/front.jpg" alt="Dolce 8" />
         </div>
       </div>
       <div className="intro-text">
